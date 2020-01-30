f9dd50efff5ee81cdc173bf44855a595eaade0ac
adrianlimcy <adrianlimcy@gmail.com>
2020-01-30 17:30:24 +0800

    Assignment about components done

---
+++ Tutorial_exercises/Exercise 6/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="container">
+      <customHeader/>
+      <hr>
+      <server-list/>
+      <hr>
+    <customFooter/>
+  </div>
+</template>
+
+<script>
+import CustomHeader from './CustomHeader.vue'
+import CustomFooter from './CustomFooter.vue'
+import ServerList from './ServerFarm.vue'
+
+export default {
+  components: {
+    customHeader: CustomHeader,
+    customFooter: CustomFooter,
+    'server-list': ServerList
+  }
+
+}
+</script>
+
+<style>
+
+</style>

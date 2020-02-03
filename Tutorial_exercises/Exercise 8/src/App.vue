3c540e96a7ec62337b4d555327703e43b1d88d4d
adrianlimcy <adrianlimcy@gmail.com>
2020-02-03 18:54:34 +0800

    Exercise 8 is done

---
+++ Tutorial_exercises/Exercise 8/src/App.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12">
+                <br>
+                <button @click="color='appBlue'" class="btn btn-primary">Load Blue Template</button>
+                <button @click="color='appGreen'" class="btn btn-success">Load Green Template</button>
+                <button @click="color='appRed'" class="btn btn-danger">Load Red Template</button>
+                <hr>
+                <component :is="color">
+                  <span slot="blue">blue</span><span slot='green'>green</span><span slot='red'>red</span>
+                </component>
+                <!-- <p>{{color}}</p> -->
+                <!-- <app-blue>This is Blue</app-blue>
+                <app-green>This is Green</app-green>
+                <app-red>This is Red</app-red> -->
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import Blue from './components/Blue.vue';
+    import Green from './components/Green.vue';
+    import Red from './components/Red.vue';
+
+    export default {
+      data() {
+        return {
+          color: 'appBlue'
+        }
+      },
+      components: {
+          appBlue: Blue,
+          appGreen: Green,
+          appRed: Red
+      }
+    }
+</script>
+
+<style>
+</style>
---
+++ section_9/src/components/New.vue
@@ -0,0 +1,27 @@
+<template lang="html">
+  <div>
+    <h3>New Quote</h3>
+    <button @click="counter++" type="button" name="button">Increase!</button>
+    <p>{{counter}}</p>
+  </div>
+
+</template>
+
+<script>
+export default {
+  data(){
+    return {
+      counter: 0
+    }
+  },
+  destroyed() {
+    console.log('destroyed');
+  },
+  deactivated() {
+    console.log('deactived');
+  },
+  activated(){
+    console.log('activated');
+  }
+}
+</script>

fe2c84b0100881fd5e4a1bfa8a7d57797ed877d3
adrianlimcy <adrianlimcy@gmail.com>
2020-02-08 13:43:55 +0800

    section 12 done

--- Tutorial_exercises/Exercise 10/src/App.vue
+++ Tutorial_exercises/Exercise 10/src/App.vue
@@ -2,10 +2,16 @@
     <div class="container">
         <div class="row">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-                <h1 v-appListen:click="alertMe">Directives Exercise</h1>
-                <!-- Exercise -->
-                <!-- Build a Custom Directive which works like v-on (Listen for Events) -->
+              <h1>Directive Exercise</h1>
+              <!-- <button v-appListen="alertMe" class="btn btn-primary">Button without modifier</button> -->
+              <button v-appListen:click="alertMe" class="btn btn-warning">Button with modifier</button>
+              <!-- Exercise -->
+              <!-- Build a Custom Directive which works like v-on (Listen for Events) -->
             </div>
+        </div>
+        <hr>
+        <div class="" style="width:100px; height:100px; background-color: lightgreen" v-appListen:mouseenter="mouseEnter" v-appListen:mouseleave="mouseLeft">
+
         </div>
     </div>
 </template>
@@ -15,14 +21,23 @@
     methods: {
       alertMe() {
         console.log('triggered');
+      },
+      mouseEnter() {
+        console.log('mouse entered');
+      },
+      mouseLeft() {
+        console.log('mouse left');
       }
     },
     directives: {
       appListen: {
         bind(el, binding, vnode){
+          // el.onclick = function() {
+          //   binding.value()
+
           el.addEventListener(binding.arg, binding.value)
-          // binding.arg = click
-          // binding.value = alertMe
+            // binding.arg = click
+            // binding.value = alertMe
         }
 
       }

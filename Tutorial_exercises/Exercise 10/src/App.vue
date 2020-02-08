71c106ab5954014ac799cf9f6d3e0a93654b33a1
adrianlimcy <adrianlimcy@gmail.com>
2020-02-08 11:15:43 +0800

    exercise 10 done

---
+++ Tutorial_exercises/Exercise 10/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                <h1 v-appListen:click="alertMe">Directives Exercise</h1>
+                <!-- Exercise -->
+                <!-- Build a Custom Directive which works like v-on (Listen for Events) -->
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+  export default {
+    methods: {
+      alertMe() {
+        console.log('triggered');
+      }
+    },
+    directives: {
+      appListen: {
+        bind(el, binding, vnode){
+          el.addEventListener(binding.arg, binding.value)
+          // binding.arg = click
+          // binding.value = alertMe
+        }
+
+      }
+    }
+  }
+</script>
+
+<style>
+</style>

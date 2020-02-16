9062bc6759871fe386aefa0a9b2ee985ef8b30df
adrianlimcy <adrianlimcy@gmail.com>
2020-02-16 21:45:43 +0800

    section 16 on routing done ... a lot to absorb

---
+++ section_16/src/components/Header.vue
@@ -0,0 +1,20 @@
+<template lang="html">
+  <ul class="nav nav-pills">
+    <!-- <li class="nav-item"> -->
+      <router-link to="/" tag="li" active-class="active" exact><a>Home</a></router-link>
+    <!-- </li> -->
+    <!-- <li class="nav-item"> -->
+      <router-link to="/user" tag="li" active-class="active" exact><a>User</a></router-link>
+    <!-- </li> -->
+    <!-- <router-link to="/user/2" tag="li" active-class="active" exact><a>User 2</a></router-link> -->
+  </ul>
+
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="css" scoped>
+</style>
